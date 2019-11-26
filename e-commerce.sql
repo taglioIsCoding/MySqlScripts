5fa1ce68064b121810094a47e238370726da609b
Michele Tagliani <giantaglio@gmail.com>
2019-11-26 10:43:23 +0100

    Update e-commerce

--- e-commerce.sql
+++ e-commerce.sql
@@ -1,4 +1,5 @@
 /**
+ LOGICAL MODEL
 clienti(credito, cap, citta,via, numero c, id_cliente)
 ordini (id_ordine, data_ordine, data_inoltro, stato, data_ricezione, data_consegna, totale, id_cliente, id_corriere)
 prodotti (id_prodotto, nome, peso, qta)
@@ -80,3 +81,36 @@ create table contiene (
     foreign key (id_prodotto) references prodotti(id_prodotto)
 
 );
+
+
+/* popolo tabelle*/
+
+INSERT INTO `stato`(`id_stato`, `descrizione`) VALUES (1,"Presa in carico");
+INSERT INTO `stato`(`id_stato`, `descrizione`) VALUES (2,"In attesa del corriere");
+INSERT INTO `stato`(`id_stato`, `descrizione`) VALUES (3,"Spedito");
+INSERT INTO `stato`(`id_stato`, `descrizione`) VALUES (4,"Conseganto");
+
+
+INSERT INTO `corrieri`(`id_corriere`, `nome`, `cognome`, `nr_patente`) VALUES (1,"Ali", "Ala", 1111111111);
+INSERT INTO `corrieri`(`id_corriere`, `nome`, `cognome`, `nr_patente`) VALUES (1,"Moamed", "Ali", 2222222222);
+
+INSERT INTO `prodotti`(`id_prodotto`, `nome`, `peso`, `quantità`) VALUES (1,"Iphone XS", 100.2, 100);
+INSERT INTO `prodotti`(`id_prodotto`, `nome`, `peso`, `quantità`) VALUES (2,"Air Pods", 15.2, 340);
+INSERT INTO `prodotti`(`id_prodotto`, `nome`, `peso`, `quantità`) VALUES (3,"Mac pro", 1032.2, 23);
+INSERT INTO `prodotti`(`id_prodotto`, `nome`, `peso`, `quantità`) VALUES (4,"Ipad pro", 342.2, 59);
+
+INSERT INTO `clienti`(`id_cliente`, `via`, `cap`, `citta`,`numero_civico`,`credito`) VALUES (1,"via rossi", 25100 ,"Brescia", 100, 0.05);
+INSERT INTO `clienti`(`id_cliente`, `via`, `cap`, `citta`,`numero_civico`,`credito`) VALUES (2,"via bianchi", 25100 ,"Brescia", 30, 100000.05);
+INSERT INTO `clienti`(`id_cliente`, `via`, `cap`, `citta`,`numero_civico`,`credito`) VALUES (3,"via verdi", 25100 ,"Brescia", 20, 10);
+INSERT INTO `clienti`(`id_cliente`, `via`, `cap`, `citta`,`numero_civico`,`credito`) VALUES (4,"via verdi", 25100 ,"Brescia", 40, 700);
+
+INSERT INTO `ordini`(`id_ordine`, `data_oridne`, `data_inoltro`, `id_stato`,`data_ricezione`,`data_consegna`, `id_cliente`,`id_corriere`) VALUES (1,2019-10-20, 2019-10-21, 1, 2019-10-22, 2019-10-23, 1, 1);
+INSERT INTO `ordini`(`id_ordine`, `data_oridne`, `data_inoltro`, `id_stato`,`data_ricezione`,`data_consegna`, `id_cliente`,`id_corriere`) VALUES (2,2019-10-20, 2019-10-21, 2, 2019-10-22, 2019-10-23, 2, 2);
+INSERT INTO `ordini`(`id_ordine`, `data_oridne`, `data_inoltro`, `id_stato`,`data_ricezione`,`data_consegna`, `id_cliente`,`id_corriere`) VALUES (3,2019-10-21, 2019-10-22, 3, 2019-10-23, 2019-10-24, 3, 3);
+INSERT INTO `ordini`(`id_ordine`, `data_oridne`, `data_inoltro`, `id_stato`,`data_ricezione`,`data_consegna`, `id_cliente`,`id_corriere`) VALUES (4,2019-10-21, 2019-10-22, 4, 2019-10-23, 2019-10-24, 4, 4);
+
+INSERT INTO `contiene`(`id_ordine`, `id_prodotto`) VALUES (1,1);
+INSERT INTO `contiene`(`id_ordine`, `id_prodotto`) VALUES (2,2);
+INSERT INTO `contiene`(`id_ordine`, `id_prodotto`) VALUES (3,2);
+INSERT INTO `contiene`(`id_ordine`, `id_prodotto`) VALUES (4,3);
+
