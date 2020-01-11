596c108cb1ce29b4e5194694373ea131fe803b30
Michele Tagliani <45893037+taglioIsCoding@users.noreply.github.com>
2020-01-11 09:22:47 +0100

    Update universita.sql

--- universita.sql
+++ universita.sql
@@ -45,3 +45,53 @@ create table esami(
   foreign key (matricola) references studenti(matricola),
   foreign key (codCorso) references corsi(codCorso)
 );
+
+
+INSERT INTO `studenti`(`matricola`, `nomeS`, `congnomeS`, `CittaRes`, `Sesso`, `numTelefono`) VALUES (1,"Lorenzo","Cagni","Brescia", true, 332121321312);
+INSERT INTO `studenti`(`matricola`, `nomeS`, `congnomeS`, `CittaRes`, `Sesso`, `numTelefono`) VALUES (2,"Matteo","Huqi","Flero", true, 312312312);
+INSERT INTO `studenti`(`matricola`, `nomeS`, `congnomeS`, `CittaRes`, `Sesso`, `numTelefono`) VALUES (3,"Stefano","Valloncini","Castegnato", true, 33546571321312);
+INSERT INTO `studenti`(`matricola`, `nomeS`, `congnomeS`, `CittaRes`, `Sesso`, `numTelefono`) VALUES (4,"Matteo","Botti","Botticino", true, 45212);
+INSERT INTO `studenti`(`matricola`, `nomeS`, `congnomeS`, `CittaRes`, `Sesso`, `numTelefono`) VALUES (5,"Giulio","Lonati","Roma", true, 145212);
+
+INSERT INTO `docenti`(`matricolaD`, `nomeD`, `congnomeD`, `dipartimento`, `stipendio`) VALUES (1,"Barbara","Bottari","Informatica", 3321312);
+INSERT INTO `docenti`(`matricolaD`, `nomeD`, `congnomeD`, `dipartimento`, `stipendio`) VALUES (2,"Tobia","Donato","Informatica", 3312);
+INSERT INTO `docenti`(`matricolaD`, `nomeD`, `congnomeD`, `dipartimento`, `stipendio`) VALUES (3,"Paolo","Lancello","sms", 1000);
+
+INSERT INTO `corsi`(`codCorso`, `nomeC`, `numCrediti`, `matricolaDoc`) VALUES (1,"Informatica",6,2);
+INSERT INTO `corsi`(`codCorso`, `nomeC`, `numCrediti`, `matricolaDoc`) VALUES (2,"Inglese", 20, 1);
+INSERT INTO `corsi`(`codCorso`, `nomeC`, `numCrediti`, `matricolaDoc`) VALUES (3,"Tecnologie",4,3);
+
+INSERT INTO `esami`(`codCorso`, `matricola`, `voto`) VALUES (1,2,4);
+INSERT INTO `esami`(`codCorso`, `matricola`, `voto`) VALUES (3,2,14);
+INSERT INTO `esami`(`codCorso`, `matricola`, `voto`) VALUES (2,1,7);
+INSERT INTO `esami`(`codCorso`, `matricola`, `voto`) VALUES (1,3,30);
+INSERT INTO `esami`(`codCorso`, `matricola`, `voto`) VALUES (3,4,20);
+INSERT INTO `esami`(`codCorso`, `matricola`, `voto`) VALUES (2,5,21);
+
+
+
+/*Esempi query*/
+SELECT matricola  FROM `studenti` WHERE 1 ORDER BY matricola DESC
+SELECT MAX(stipendio) as top from docenti
+SELECT * FROM `studenti` WHERE cittaRes LIKE "b%"
+
+SELECT studenti.matricola,
+			studenti.congnomeS,
+            studenti.nomeS
+            	from studenti, esami
+                where studenti.matricola = esami.matricola and
+                esami.voto > 10 AND
+                studenti.congnomeS LIKE "b%"
+
+SELECT studenti.matricola,
+			studenti.congnomeS,
+            studenti.nomeS
+            	from studenti inner join esami on studenti.matricola = esami.matricola
+                where
+                esami.voto > 10 AND
+                studenti.congnomeS LIKE "b%"
+                
+SELECT studenti.congnomeS
+	from studenti join esami join docenti
+    where esami.voto > 18
+    		AND docenti.stipendio>100
