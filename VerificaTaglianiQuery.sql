01278ff9e2040ee5fd3761d08ec15de9b038d7f5
Michele Tagliani <giantaglio@gmail.com>
2020-01-31 21:19:17 +0100

    Add VerificaSQl

---
+++ VerificaTaglianiQuery.sql
@@ -0,0 +1,73 @@
+create database xxxxx;
+
+create table XXXX (
+ XXX varchar(20),
+ XXXX date,
+ XXXXX varchar(30),
+
+ primary key(XXXX),
+ foreign key (XXXX) references XXXX(XXXX)
+);
+
+create table XXXXX (
+  XXXX varchar(20),
+  XXXX date,
+  XXXX varchar(30),
+
+  primary key(XXXX),
+  foreign key (XXXX) references XXXXX (XXXX)
+);
+
+create table XXXX (
+  XXX varchar(20),
+  XXXX varchar(30),
+  XXXXX int(4),
+
+  primary key(XXXXX),
+  foreign key (XXXXX) references XXXX (XXXXX)
+
+);
+
+create table XXXXX (
+XXXXX varchar(20),
+XXXX varchar(20),
+XXXXX varchar(20),
+
+primary key (XXXX),
+foreign key (XXXXXX) references XXXXX(XXXX),
+foreign key (XXXX) references XXXX(xXXXX)
+);
+
+create table XXXXX (
+ XXXX varchar(20),
+ XXXX varchar(20),
+ XXXXX int (5),
+ XXXX int(10),
+
+primary key(XXXX),
+ foreign key (XXXX) references XXXX(xXXXX)
+);
+
+create table XXXX(
+XXXX varchar(20),
+   XXXX varchar(20),
+   XXXX varchar(20),
+
+   PRIMARY key (XXXX),
+   FOREIGN KEY (XXXX) REFERENCES XXXX(XXXX),
+   FOREIGN key (XXXX) REFERENCES XXXX(XXX)
+);
+
+
+INSERT INTO `XXXXX`(`XXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES ("XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES ("XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES ("XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES ("XXXXX","XXXXX", XXXXX, XXXXX);
+
+
+INSERT INTO `XXXXX`(`XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES (XXXXX,"XXXXX","XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES (XXXXX,"XXXXX","XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES (XXXXX,"XXXXX","XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES (XXXXX,"XXXXX","XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES (XXXXX,"XXXXX","XXXXX","XXXXX", XXXXX, XXXXX);
+INSERT INTO `XXXXX`(`XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`, `XXXXX`) VALUES (XXXXX,"XXXXX","XXXXX","XXXXX", XXXXX, XXXXX);
