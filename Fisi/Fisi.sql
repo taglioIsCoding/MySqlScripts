41a4cbc6d70295a24c137852f209c1378d9d0bd3
Michele Tagliani <giantaglio@gmail.com>
2020-05-01 12:22:27 +0200

    Update fisi.sql

--- Fisi/Fisi.sql
+++ Fisi/Fisi.sql
@@ -35,7 +35,7 @@ CREATE TABLE Rider
   Nome VARCHAR(20),
   Cognome VARCHAR(20),
   Sesso BOOLEAN,
-  Stance VARCHAR(6),
+  Stance VARCHAR(9),
   DataNascita DATE,
   ID_Nazione INT,
   PRIMARY KEY (ID_Rider),
@@ -73,12 +73,12 @@ INSERT INTO Nazionale(ID_Nazione, Nazione) VALUES (3, "Canada");
 INSERT INTO Nazionale(ID_Nazione, Nazione) VALUES (4, "Belgio"); 
 INSERT INTO Nazionale(ID_Nazione, Nazione) VALUES (5, "Norvegia"); 
 
-INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (1, 2020-1-13, "BigAir", 10000, "Modena", "44.6568" ,"10.9202" ,1); 
-INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (2, 2020-2-15, "Canada Champ", 13400, "Montreal", "45.5088" ,"-73.561" ,3); 
-INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (3, 2020-3-16, "Belgium Open", 3455, "Bruxelles", "50.8504" ,"4.34878" ,4); 
-INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (4, 2020-4-20, "US Open", 22000, "Aspen", "39.1910" ,"-106.817" ,2); 
-INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (5, 2020-5-12, "X games", 3500, "Oslo", "59.9127" ,"10.7460" ,5); 
-INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (6, 2020-6-10, "BigAir2", 10000, "Palo Alto", "37.4683" ,"-122.14" ,2); 
+INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (1, 20200113, "BigAir", 10000, "Modena", "44.6568" ,"10.9202" ,1); 
+INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (2, 20200215, "Canada Champ", 13400, "Montreal", "45.5088" ,"-73.561" ,3); 
+INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (3, 20200316, "Belgium Open", 3455, "Bruxelles", "50.8504" ,"4.34878" ,4); 
+INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (4, 20200420, "US Open", 22000, "Aspen", "39.1910" ,"-106.817" ,2); 
+INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (5, 20200512, "X games", 3500, "Oslo", "59.9127" ,"10.7460" ,5); 
+INSERT INTO Gara(ID_Gara, DataGara, Nome, PrimoPremio, Citta, Latitudine, Longitudine, ID_Nazione) VALUES (6, 20200610, "BigAir2", 10000, "Palo Alto", "37.4683" ,"-122.14" ,2); 
 
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (1, "Ollie", null); 
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (2, "1440", "FS"); 
@@ -95,21 +95,21 @@ INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (12, "Rodeo", "FS");
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (13, "Michalchuk", "FS"); 
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (14, "Michalchuk", "BS"); 
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (15, "Cork", "BS"); 
-INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (16 "Double Cork", "BS"); 
+INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (16, "Double Cork", "BS"); 
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (17, "Triple Cork", "BS"); 
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (18, "Quad Cork", "BS"); 
 INSERT INTO Trick(ID_Trick, Nome, Rotazione) VALUES (19, "Rodeo", "BS"); 
 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (1, "Mark", "McMorris", true, "Regular", 1993-12-9 , 3); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (2, "Max", "Parrot", true, "Regular", 1993-12-9 , 3); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (3, "Red", "Gerard", true, "Goofy", 1993-12-9 , 2); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (4, "Alberto", "Maffei", true, "Regular", 1993-12-9 , 1); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (5, "Seppe", "Smith", true, "Goofy", 1993-12-9 , 4); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (6, "Chris", "Corning", true, "Goofy", 1993-12-9 , 2); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (7, "Eero", "Ettala", true, "Regular", 1993-12-9 , 5); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (8, "Marcus", "Kleveland", true, "Regular", 1993-12-9 , 5); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (9, "Roberto", "Bragotto", true, "Goofy", 1993-12-9 , 1); 
-INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (10, "Vlad", "Hakink", true, "Regular", 1993-12-9 , 5); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (1, "Mark", "McMorris", true, "Regular", 19931209 , 3); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (2, "Max", "Parrot", true, "Regular", 19940606 , 3); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (3, "Red", "Gerard", true, "Goofy", 20000629 , 2); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (4, "Alberto", "Maffei", true, "Regular", 19950511 , 1); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (5, "Seppe", "Smith", true, "Goofy", 19910713 , 4); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (6, "Chris", "Corning", true, "Goofy", 19990907 , 2); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (7, "Eero", "Ettala", true, "Regular", 19841017 , 5); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (8, "Marcus", "Kleveland", true, "Regular", 19990425 , 5); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (9, "Roberto", "Bragotto", true, "Goofy", 19911009 , 1); 
+INSERT INTO Rider(ID_Rider, Nome, Cognome, Sesso, Stance, DataNascita, ID_Nazione) VALUES (10, "Vlad", "Hakink", true, "Regular", 19981212 , 5); 
 
 INSERT INTO Partcipare(ID_Gara, ID_Rider, Vincitore) VALUES (1, 1, "Y"); 
 INSERT INTO Partcipare(ID_Gara, ID_Rider, Vincitore) VALUES (1, 3, "N"); 
